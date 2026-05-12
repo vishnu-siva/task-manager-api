27b633da31a17b9a2df63c183998ccd2aae82bc0
Vishnuha Sivanandarajah <vishnuhasivanandarajah8855@gmail.com>
2026-05-12 22:56:33 +0530

    feat: add MongoDB connection and authentication models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
